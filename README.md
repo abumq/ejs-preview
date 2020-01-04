97ae4e10a9000387ff3bee9f08bc58670cf4b891
abumusamq <mkhan3189@gmail.com>
2020-01-04 16:23:55 +1100

    original code

---
+++ README.md
@@ -0,0 +1,59 @@
+# EJS Preview
+
+[![Donate](https://amrayn.github.io/donate.png?v2)](https://amrayn.com/donate)
+
+```
+yarn global add ejs-preview
+```
+
+```
+ejs-preview --root=/path/to/root/files --common=/json/file.json
+```
+
+* `root`: Path to the root files. e.g, `/app/src/templates`
+* `common`: Path to JSON file. Common data for all the templates. e.g, `/app/src/templates/preview-common.json`
+
+Once server is running you can pass following query parameters
+
+* `template`: EJS file base to render (relative to root) e.g, `?template=emails/welcome` will render `<root>/emails/welcome.ejs`.
+
+  The template specific data is picked up from `<root>/emails/welcome.preview.js` which should look like:
+
+```
+module.exports = {
+  activationLink: '/link',
+};
+```
+* `base` If you want to override `base` args provided at the startup
+* `plain` If specified `plain/text` content-type is sent
+
+You can change the port with `PORT` environment variable
+
+
+# License
+```
+The MIT License (MIT)
+
+(c) 2020 Amrayn Web Services
+(c) 2020 @abumusamq
+
+https://amrayn.com
+https://humble.js.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+```
---
+++ index.js
@@ -0,0 +1,99 @@
+/**
+ * Bismillah ar-Rahmaan ar-Raheem
+ *
+ * ejs-preview
+ *
+ * (c) 2020 Amrayn Web Services
+ * (c) 2020 @abumusamq
+ *
+ * This library is released under the MIT Licence.
+ *
+ * https://amrayn.com
+ * https://humble.js.org
+ */
+
+const fs = require('fs');
+const pathResolve = require('path').resolve;
+const express = require('express');
+const ejs = require('ejs');
+const minimist = require('minimist');
+const chalk = require('chalk');
+
+const args = minimist(process.argv);
+
+const templateRoot = args.root; // root directory of template
+
+if (!templateRoot) {
+  console.error(chalk.red('Template root not specified.'));
+  process.exit(1);
+}
+
+console.log('Template root %s', templateRoot);
+
+if (!fs.existsSync(pathResolve(templateRoot))) {
+  console.error(chalk.red('Template root [%s] does not exist.'), pathResolve(templateRoot));
+  process.exit(1);
+}
+
+const previewBaseData = args.common;
+if (!previewBaseData) {
+  console.warn(chalk.yellow('WARNING: Preview base data not specified (--base) - you will need to specify it with each request'));
+}
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const cleanRequire = f => {
+  delete require.cache[require.resolve(f)];
+  return require(f);
+}
+
+app.get('/', (req, res) => {
+  const baseDataFile = fs.existsSync(`${templateRoot}/${req.query.base}.js`) ? `${templateRoot}/${req.query.base}.js`
+    : previewBaseData;
+
+  console.debug('Base data file %s', baseDataFile);
+
+  const baseData = baseDataFile ? cleanRequire(baseDataFile) : {};
+
+  const ejsFile = req.query.template || template;
+
+  const previewFilename = `${templateRoot}/${req.query.template}.preview.js`;
+  const templateDataFile = fs.existsSync(pathResolve(previewFilename)) ? pathResolve(previewFilename)
+    : null;
+
+  console.debug('Template data file %s', previewFilename);
+
+  const templateData = templateDataFile ? cleanRequire(templateDataFile) : {};
+  const templateFile = `${templateRoot}/${ejsFile}.ejs`;
+  const isPlain = typeof req.query.plain !== 'undefined';
+
+  ejs.renderFile(pathResolve(templateFile), {
+    base: baseData,
+    ...templateData,
+  }, { async: false }, (err, str) => {
+    if (err) {
+      console.error(err);
+      res.setHeader('Content-Type', 'text/html');
+      res.send('Render error<br/>' + err);
+    } else {
+      if (isPlain) {
+        res.setHeader('Content-Type', 'text/plain');
+      } else {
+        res.setHeader('Content-Type', 'text/html');
+      }
+      res.send(str);
+    }
+  });
+});
+
+app.listen(port, () => console.log(`
+  EJS template preview available on ${port}!
+  ----
+  POSSIBLE QUERY PARAMETERS
+
+  http://localhost:${port}?template=emails/welcome
+  http://localhost:${port}?template=emails/welcome-plain&plain
+  http://localhost:${port}?base=emails/welcome-plain&plain
+  http://localhost:${port}?base=custom-base
+`))
